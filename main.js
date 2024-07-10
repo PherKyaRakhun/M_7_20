3661dfea41b9d03eb57ac8f7bf4718878c5a2c42
PherKyaRakhun <rajpootfaizan992@gmail.com>
2024-07-10 11:06:38 +0500

    commit for b5

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-// jkhscsdc
 
-
-//scsdjkbckdc
+void b5(Process p, int timeQuantum) {
+    p.remainingTime -= Math.min(p.remainingTime, timeQuantum);
+    if (p.remainingTime > 0) processQueue.add(p);  // Re-add process if not finished
+}

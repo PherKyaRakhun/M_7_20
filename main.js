8b0884b828d0b3965382e3dc81c402a648e3a58b
PherKyaRakhun <rajpootfaizan992@gmail.com>
2024-07-10 11:04:18 +0500

    commit for b4

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-//m,y name is raza
 
-
+void b4(Process p, int timeQuantum) {
+    p.remainingTime -= Math.min(p.remainingTime, timeQuantum);
+    if (p.remainingTime > 0) processQueue.add(p);  // Re-add process if not finished
+}
